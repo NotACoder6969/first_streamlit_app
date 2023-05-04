204a6220b42ebbb60c58943a404dba91021bd969
NotACoder6969 <116000454+NotACoder6969@users.noreply.github.com>
2023-05-04 17:18:31 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,12 @@
 import pandas
+import streamlit
 import requests
 import snowflake.connector
 from urllib.error import URLError
 
 
-
-streamlit.header("Breakfast Menu")
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
