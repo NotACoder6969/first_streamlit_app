77e78f7e8c76250d02ed9f0a62c0af37b56fed83
NotACoder6969 <116000454+NotACoder6969@users.noreply.github.com>
2023-05-04 17:16:39 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from urllib.error import URLError
 
 
 
-streamlit.header('Breakfast Menu')
+streamlit.header("Breakfast Menu")
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
