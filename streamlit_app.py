cbbb0b27dbfeca21af7a78801e118de865b2a5df
NotACoder6969 <116000454+NotACoder6969@users.noreply.github.com>
2023-08-10 16:13:18 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -169,10 +169,10 @@ with tab1:
                     card(title=str(total_revenue), text='Total Sales Revenue Generated')
                     Improve_revenue=(total_revenue-475309.4)
                     if Improve_revenue < 0:
-                         improvement=((round(total_revenue/ 475309.4, 2))-1)*100+("%")
+                         improvement=((round(total_revenue/ 475309.4, 2))-1)*100
                          card_title = 'Total Sales Revenue Loss'
                     else:
-                         improvement=(1-(round(total_revenue / 475309.4, 2)))*100+("%")
+                         improvement=(1-(round(total_revenue / 475309.4, 2)))*100
                          card_title = 'Total Sales Revenue Gain'
                     st.metric(card_title,Improve_revenue,improvement)
                elif city=="San Mateo":
